59b0c9ba48a22facb65c24e4a04a952b00f2df99
Kai Berggold <mail@kaiberggold.de>
2024-02-26 21:58:45 +0000

    add submodules

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -1,16 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "USART.h"
+//#include "USART.h"
 
 int main()
 {
-    initUSART();
+    //initUSART();
   
      
     // Set built-in LED pin as output
     DDRB |= (1 << DDB5);
     while (1) {
-    printString("Hallo Kai!\r\n");  
+    //printString("Hallo Kai!\r\n");  
         PORTB |=  (1 << PB5);   // LED on
         _delay_ms(100);
         PORTB &= ~(1 << PB5);   // LED off
