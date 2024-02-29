ec55011fa54b513f43db56ca3cf65ac94efff4ea
Kai Berggold <mail@kaiberggold.de>
2024-02-29 17:28:01 +0000

    usart working

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -2,13 +2,21 @@
 #include "usart_dbg.h"
 #include "digital_out.h"
 
+
+
+
+
 int main()
 {
    
     utils::UsartDbg dbg;
     dbg.usart_dbg_init();
     _delay_ms(100);
-    dbg.usart_transmit_byte(65);
+    dbg.usart_transmit_byte(64);
+    for(int i=0;i<26;i++)
+    {
+        dbg.print_ascii(65+i);
+    }
     const utils::DigitalOut<mcal::r::port_b,5> led;
      std::uint8_t i=0;
      for (;;)
