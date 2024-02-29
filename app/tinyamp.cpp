806ab4dcb58a1d576da7a8af8a963afcda873915
Kai Berggold <mail@kaiberggold.de>
2024-02-29 14:19:17 +0000

    usart

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -6,12 +6,22 @@ int main()
 {
    
     utils::UsartDbg dbg;
-    
+    dbg.usart_dbg_init();
+    _delay_ms(100);
+    dbg.usart_transmit_byte(65);
     const utils::DigitalOut<mcal::r::port_b,5> led;
-    
-    for (;;)
-    {
-        _delay_ms(500);
+     std::uint8_t i=0;
+     for (;;)
+     {
+        if (i==10)
+        {
+            dbg.print_ascii(65);
+            i=0;
+        }
+            i++;
+        _delay_ms(100);
+        
+        dbg.usart_dbg_step();
         led.set_bit(false);
         _delay_ms(100);
         led.set_bit(true);
