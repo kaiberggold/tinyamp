31db4f1fb74c97a633843bab517cc880e908d28e
Kai Berggold <mail@kaiberggold.de>
2024-03-03 00:21:26 +0000

    change formatter

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -2,19 +2,23 @@
 #include <cstdint>
 #include <util/delay.h>
 
-int main() {
+int main()
+{
 
   utils::UsartDbg dbg(9600);
   _delay_ms(100);
   dbg.usart_transmit_byte(64);
-  for (int i = 0; i < 26; i++) {
+  for (int i = 0; i < 26; i++)
+  {
     dbg.print_ascii(65 + i);
   }
   utils::DigitalPort<std::uint8_t, std::uint8_t, 0> portb;
   portb.set_to_out_mask(1U << 5U);
   std::uint8_t i = 0;
-  for (;;) {
-    if (i == 10) {
+  for (;;)
+  {
+    if (i == 10)
+    {
       dbg.print_ascii(65);
       i = 0;
     }
