5010c05ecce41b2e26cd5c66df0d0640f4b5a4c1
Kai Berggold <mail@kaiberggold.de>
2024-03-03 00:15:45 +0000

    formatter

--- include/tinyamp.h
+++ include/tinyamp.h
@@ -1,21 +1,19 @@
 // enum portidx
-DigitalPort<std::uint8_t,std::uint8_t, 0>  port_b;
-DigitalPin(port_b,3) led;
+DigitalPort<std::uint8_t, std::uint8_t, 0> port_b;
+DigitalPin(port_b, 3) led;
 
-UsartCom(0,9600) usart;
-ByteBufferCom(usart,256) usart_byte_buffer;
+UsartCom(0, 9600) usart;
+ByteBufferCom(usart, 256) usart_byte_buffer;
 SerialDbg(usart) dbg;
 
-I2CCom(0,100) i2c;
-ByteBufferCom(i2c,200) i2c_buffer;
+I2CCom(0, 100) i2c;
+ByteBufferCom(i2c, 200) i2c_buffer;
 
-PotiIc(MPC4x,0b01000'0100,i2c_buffer) poti_ic1;
-DigiPot(poti_ic1,0) poti_volume;
-DigiPot(poti-ic2,1) poti_gain,
+PotiIc(MPC4x, 0b01000'0100, i2c_buffer) poti_ic1;
+DigiPot(poti_ic1, 0) poti_volume;
+DigiPot(poti - ic2, 1) poti_gain,
 
-DigitalPin(port_b) r1;
+    DigitalPin(port_b) r1;
 DigitalPin(port_b) r2;
 
-RotaryDecoder(r1,r2) rotary_volume;
-
-
+RotaryDecoder(r1, r2) rotary_volume;
