d452939af7b8bf1f14badb685186bcee3f13f7e5
Kai Berggold <mail@kaiberggold.de>
2024-03-21 23:29:20 +0000

    debug

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -1,13 +1,13 @@
 #include "tinylib_utils.h"
 #include <cstdint>
 #include <util/delay.h>
-
+#undef SERIAL_DBG
 int main()
 {
 
   utils::I2cCom<std::uint8_t, std::uint8_t, 0> i2c(UINT32_C(100000));
-  utils::PotiIc<std::uint8_t, std::uint8_t, 0> poti_ic_1(1, 2, &i2c);
-  utils::DigiPoti<std::uint8_t, std::uint8_t, 0> poti_1(poti_ic_1, 1);
+  utils::PotiIc<std::uint8_t, std::uint8_t, 0> poti_ic_1(88, 0, &i2c);
+  utils::DigiPoti<std::uint8_t, std::uint8_t, 0> poti_1(poti_ic_1, 0);
   i2c.init();
 #ifdef SERIAL_DBG
   utils::UsartDbg dbg(9600);
@@ -17,25 +17,27 @@ int main()
     dbg.print_ascii(65 + i);
   }
 #endif
-  utils::DigitalPort<std::uint8_t, std::uint8_t, 0> portb;
-  portb.set_to_out_mask(1U << 5U);
+  // utils::DigitalPort<std::uint8_t, std::uint8_t, 0> portb;
+  // portb.set_to_out_mask(1U << 5U);
   std::uint8_t i = 0;
   for (;;)
   {
-    if (i == 10)
-    {
-      dbg.print_ascii(65);
-      i = 0;
-    }
-    i++;
-    _delay_ms(50);
+    // if (i == 10)
+    // {
+    //   dbg.print_ascii(65);
+    //   i = 0;
+    // }
+    // i++;
+    // _delay_ms(50);
 
-    dbg.usart_dbg_step();
-    portb.set_port(1U << 5U);
-    _delay_ms(50);
-    portb.set_port(0U);
-    poti_1.set_volatile(44);
+    // dbg.usart_dbg_step();
+    // portb.set_port(1U << 5U);
+    poti_1.set_volatile(20);
+    i2c.flush_blocking();
     _delay_ms(1000);
-    poti_1.set_volatile(94);
+    // portb.set_port(0U);
+    poti_1.set_volatile(100);
+    i2c.flush_blocking();
+    _delay_ms(1000);
   }
 }
