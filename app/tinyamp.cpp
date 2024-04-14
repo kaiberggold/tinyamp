13042db7ab462b3d5f8c6a41759a011a5e73e7de
Kai Berggold <mail@kaiberggold.de>
2024-04-14 08:27:07 +0200

    timer interrupts

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -1,11 +1,21 @@
 #include "tinylib_utils.h"
 #include <cstdint>
 #include <util/delay.h>
-#include "../include/interrupts.h"
-// #include <avr/io.h>
-// #undef SERIAL_DBG
+// #include <avr/interrupt.h>
+#include "../include/interrupt_vectors.h"
+//  #include <avr/io.h>
+//  #undef SERIAL_DBG
+
+// ISR(TIMER1_OVF_vect)
+// {
+//   hal::HalDigitalPort<std::uint8_t, std::uint8_t, 0>::reg_xor(1U << 5);
+//   hal::HalRegAccess<std::uint8_t, std::uint16_t, hal::TCNT134[0]>::reg_set(100U);
+// }
+
 int main()
 {
+  utils::Interrupts::enable_all_interrupts();
+  utils::Timer<std::uint8_t, std::uint8_t, 0>::init();
   constexpr std::uint32_t i2c_freq = UINT32_C(100000);
   constexpr std::uint8_t I2C_BUS_IDX = 0;
   constexpr std::uint8_t POTI_IC_1_CHIP_SELECT_ADDRESS = 0;
@@ -28,46 +38,48 @@ int main()
 #endif
 
   // SPI
-  constexpr std::uint8_t clock_scaling = 16;
-  constexpr std::uint8_t spi_idx = 0;
-  utils::DigitalPin<std::uint8_t, std::uint8_t, 0, 2> cs_ad;
-  cs_ad.set_pin(true);
-  utils::SpiCom<std::uint8_t, std::uint8_t, spi_idx, cs_ad, clock_scaling> spi;
-  spi.init();
+  // constexpr std::uint8_t clock_scaling = 16;
+  // constexpr std::uint8_t spi_idx = 0;
+  // utils::DigitalPin<std::uint8_t, std::uint8_t, 0, 2> cs_ad;
+  // cs_ad.set_pin(true);
+  // utils::SpiCom<std::uint8_t, std::uint8_t, spi_idx, cs_ad, clock_scaling> spi;
+  // spi.init();
 
   utils::DigitalPin<std::uint8_t, std::uint8_t, 0, 5> led;
   led.set_to_out_pin();
-
+  led.set_pin(false);
+  std::uint8_t i = 0;
   for (;;)
   {
-    cs_ad.set_pin(false);
-    spi.send(0x01);
-    while (spi.transmission_active())
-    {
-    }
-    spi.read();
-    spi.send(0x90);
-    while (spi.transmission_active())
-    {
-    }
-    std::uint8_t d2 = spi.read();
-    spi.send(0x00);
-    while (spi.transmission_active())
-    {
-    }
-    std::uint8_t d3 = spi.read();
-    cs_ad.set_pin(true);
-    dbg.print_hex_byte(d2);
-    dbg.print_hex_byte(d3);
-    dbg.print_ascii(10);
-    dbg.usart_dbg_flush();
+    i++;
+    // cs_ad.set_pin(false);
+    // spi.send(0x01);
+    // while (spi.transmission_active())
+    // {
+    // }
+    // spi.read();
+    // spi.send(0x90);
+    // while (spi.transmission_active())
+    // {
+    // }
+    // std::uint8_t d2 = spi.read();
+    // spi.send(0x00);
+    // while (spi.transmission_active())
+    // {
+    // }
+    // std::uint8_t d3 = spi.read();
+    // cs_ad.set_pin(true);
+    // dbg.print_hex_byte(d2);
+    // dbg.print_hex_byte(d3);
+    // dbg.print_ascii(10);
+    // dbg.usart_dbg_flush();
 
-    _delay_ms(2000);
+    // _delay_ms(2000);
     // i++;
     // led.set_pin(true);
-    // poti_1.set_volatile(i);
-    // poti_1.set_volatile(d3);
-    // i2c.flush_blocking();
+    // // poti_1.set_volatile(i);
+    // // poti_1.set_volatile(d3);
+    // // i2c.flush_blocking();
 
     // _delay_ms(500);
     // led.set_pin(false);
