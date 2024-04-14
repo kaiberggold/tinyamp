8311ec3ad756ddd38d43206d6a6b35c42a0daa80
Kai Berggold <mail@kaiberggold.de>
2024-04-14 22:53:38 +0200

    Poti_ic

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -8,27 +8,23 @@ constexpr std::uint16_t start_time = UINT16_C(0xFC00);
 //  #include <avr/io.h>
 //  #undef SERIAL_DBG
 
-// ISR(TIMER1_OVF_vect)
-// {
-//   hal::HalDigitalPort<std::uint8_t, std::uint8_t, 0>::reg_xor(1U << 5);
-//   hal::HalRegAccess<std::uint8_t, std::uint16_t, hal::TCNT134[0]>::reg_set(100U);
-// }
-
 int main()
 {
+  // generic setup
   utils::Interrupts::enable_all_interrupts();
   hal::TimerT<std::uint8_t, std::uint8_t, std::uint16_t, 0>::init(start_time);
+
   constexpr std::uint32_t i2c_freq = UINT32_C(100000);
   constexpr std::uint8_t I2C_BUS_IDX = 0;
-  constexpr std::uint8_t POTI_IC_1_CHIP_SELECT_ADDRESS = 0;
+  constexpr std::uint8_t MPC4442_1_CHIP_SELECT_ADDRESS = 0;
   constexpr std::uint8_t POTI_ID_0 = 0;
   constexpr std::uint8_t POTI_ID_3 = 3;
-  constexpr std::uint8_t POTI_CHIP_ADDRESS = 88;
+  constexpr std::uint8_t MPC4442_1_ADDRESS = 88;
 
   utils::I2cCom<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> i2c;
-  utils::Mp44xx<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> poti_ic_1(POTI_CHIP_ADDRESS, POTI_IC_1_CHIP_SELECT_ADDRESS, &i2c);
-  utils::DigiPoti<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> poti_1(poti_ic_1, POTI_ID_0);
-  utils::DigiPoti<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> poti_2(poti_ic_1, POTI_ID_3);
+  utils::Mp44xx<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> mpc4442_1(MPC4442_1_ADDRESS, MPC4442_1_CHIP_SELECT_ADDRESS, &i2c);
+  utils::DigiPoti<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> poti_1(mpc4442_1, POTI_ID_0);
+  utils::DigiPoti<std::uint8_t, std::uint8_t, I2C_BUS_IDX, i2c_freq> poti_2(mpc4442_1, POTI_ID_3);
   i2c.init();
 #ifdef SERIAL_DBG
   utils::UsartDbg dbg(9600);
