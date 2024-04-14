cd7d286d9468cbc1d36e491595be869808487544
Kai Berggold <mail@kaiberggold.de>
2024-04-14 08:30:18 +0200

    cleanup

--- include/interrupt_vectors.h
+++ include/interrupt_vectors.h
@@ -9,12 +9,9 @@ extern "C" void __vector_13(void) __attribute__((signal, used, externally_visibl
 void __vector_13(void)
 {
     hal::HalDigitalPort<std::uint8_t, std::uint8_t, 0>::reg_xor(1U << 5);
-    std::uint8_t sreg;
-    sreg = hal::HalRegAccess<std::uint8_t, std::uint8_t, hal::SREGa>::get_reg();
+
+    std::uint8_t sreg = hal::HalRegAccess<std::uint8_t, std::uint8_t, hal::SREG>::get_reg();
     hal::disable_all_interrupts();
-    /* Set TCNT1 to i */
-    // TCNT1 = 0xEE00;
-    hal::HalRegAccess<std::uint16_t, std::uint16_t, hal::TCNT134[0]>::reg_set(0xEE00);
-    /* Restore global interrupt flag */
-    hal::HalRegAccess<std::uint8_t, std::uint8_t, hal::SREGa>::reg_set(sreg);
+    hal::HalRegAccess<std::uint16_t, std::uint16_t, hal::TCNT134[0]>::reg_set(UINT16_C(0xEE00));
+    hal::HalRegAccess<std::uint8_t, std::uint8_t, hal::SREG>::reg_set(sreg);
 }
