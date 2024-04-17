974f853344ce39823e00c7568dd0676028f2c98d
Kai Berggold <mail@kaiberggold.de>
2024-04-18 00:01:18 +0200

    ad

--- app/tinyamp.cpp
+++ app/tinyamp.cpp
@@ -2,18 +2,41 @@
 #include "hal.h"
 #include <cstdint>
 #include <util/delay.h>
-// #include <avr/interrupt.h>
-constexpr std::uint16_t start_time = UINT16_C(0xFC00);
-#include "../include/interrupt_vectors.h"
-//   #include <avr/io.h>
 #undef SERIAL_DBG
+#define INTERRUPTS
+
+// #include <avr/io.h>
+// #include <avr/interrupt.h>
+
+// used in interrupt_vectors, so defined here
+constexpr std::uint16_t start_time = UINT16_C(0xFFFD);
+constexpr std::uint8_t clock_scaling = 16;
+constexpr std::uint8_t spi_idx = 0;
+constexpr std::uint8_t spi_mode = 0;
+constexpr std::uint8_t spi_cs_port = 0;
+constexpr std::uint8_t spi_cs_pin = 2;
+using ad_cs_digital_pin_t = utils::DigitalPin<std::uint8_t, std::uint8_t, spi_cs_port, spi_cs_pin>;
+using spi_static_t = utils::SpiComStatic<std::uint8_t, std::uint8_t, spi_idx, clock_scaling, spi_mode, ad_cs_digital_pin_t>;
+using Mpc3202_t = utils::Mpc3202<std::uint8_t, std::uint8_t, spi_static_t, ad_cs_digital_pin_t>;
+using AdIcStatic_t = ifc::AdIcStaticIf<Mpc3202_t, std::uint8_t, std::uint8_t, spi_static_t, ad_cs_digital_pin_t>;
+
+static volatile std::uint8_t spi0_state = 0;
+static volatile std::uint16_t ad_raw_val_0 = 0;
+
+#ifdef INTERRUPTS
+
+#include "../include/interrupt_vectors.h"
+#endif
 
 int main()
 {
-  // generic setup
-  // utils::Interrupts::enable_all_interrupts();
-  // hal::TimerT<std::uint8_t, std::uint8_t, std::uint16_t, 0>::init(start_time);
-
+  utils::DigitalPin<std::uint8_t, std::uint8_t, 0, 1> led;
+  led.set_to_out_pin();
+  led.set_pin(true);
+#ifdef INTERRUPTS
+  utils::Interrupts::enable_all_interrupts();
+  hal::TimerT<std::uint8_t, std::uint8_t, std::uint16_t, 0>::init(start_time);
+#endif
   constexpr std::uint32_t i2c_freq = UINT32_C(100000);
   constexpr std::uint8_t I2C_BUS_IDX = 0;
   constexpr std::uint8_t MPC4442_1_CHIP_SELECT_ADDRESS = 0;
@@ -39,32 +62,21 @@ int main()
 #endif
 
   // SPI
-  constexpr std::uint8_t clock_scaling = 16;
-  constexpr std::uint8_t spi_idx = 0;
-  constexpr std::uint8_t spi_mode = 0;
-  constexpr std::uint8_t spi_cs_port = 0;
-  constexpr std::uint8_t spi_cs_pin = 2;
 
-  using ad_cs_digital_pin_t = utils::DigitalPin<std::uint8_t, std::uint8_t, spi_cs_port, spi_cs_pin>;
-  // cs_ad_t cs_ad;
-  // cs_ad.set_pin(true);
-  using spi_static_t = utils::SpiComStatic<std::uint8_t, std::uint8_t, spi_idx, clock_scaling, spi_mode, ad_cs_digital_pin_t>;
-  spi_static_t spi_static;
-  spi_static.init();
+  spi_static_t::init();
+#ifdef INTERRUPTS
+  spi_static_t::enable_interrupt();
+#endif
 
-  using Mpc3202_t = utils::Mpc3202<std::uint8_t, std::uint8_t, spi_static_t, ad_cs_digital_pin_t>;
-  using AdIcStatic_t = ifc::AdIcStaticIf<Mpc3202_t, std::uint8_t, std::uint8_t, spi_static_t, ad_cs_digital_pin_t>;
   // AdIcStatic_t mp3202;
   // std::uint16_t t = mp3202.get_raw_value();
 
-  utils::DigitalPin<std::uint8_t, std::uint8_t, 0, 5>
-      led;
-  led.set_to_out_pin();
-  led.set_pin(false);
   std::uint8_t i = 0;
   for (;;)
   {
     i++;
+// #define ADMAN
+#ifdef ADMAN
     AdIcStatic_t::enable();
     _delay_us(10);
     AdIcStatic_t::template send<0>();
@@ -80,19 +92,21 @@ int main()
     std::uint8_t d3 = AdIcStatic_t::read();
 
     AdIcStatic_t::disable();
-
+#ifdef SERIAL_DBG
     dbg.print_hex_byte(d2);
     dbg.print_hex_byte(d3);
+
+    // dbg.print_ascii('A');
     dbg.print_ascii(10);
     dbg.usart_dbg_flush();
-
-    _delay_ms(2000);
+#endif
+#endif
     i++;
 
     led.set_pin(true);
-    poti_1.set_volatile(i);
-    //    poti_2.set_volatile(i);
-    i2c.flush_blocking();
+    // poti_1.set_volatile(i);
+    //     poti_2.set_volatile(i);
+    // i2c.flush_blocking();
 
     _delay_ms(500);
     led.set_pin(false);
--- include/interrupt_vectors.h
+++ include/interrupt_vectors.h
@@ -3,15 +3,37 @@
 // #include <avr/interrupt.h>
 #include <cstdint>
 
+// timer1 overflow
 extern "C" void __vector_13(void) __attribute__((signal, used, externally_visible));
-
-static volatile std::uint8_t spi0_state = 0;
 void __vector_13(void)
 {
-    spi0_state = 0;
-    // AdIcStatic_t::enable();
-
-    // AdIcStatic_t::template send<0>();
+    AdIcStatic_t::enable();
+    AdIcStatic_t::template send<0>();
     //  hal::HalDigitalPort<std::uint8_t, std::uint8_t, 0>::reg_xor(1U << 5);
-    //  hal::TimerT<std::uint8_t, std::uint8_t, std::uint16_t, 0>::irq_reset(start_time);
+    hal::TimerT<std::uint8_t, std::uint8_t, std::uint16_t, 0>::irq_reset(start_time);
+}
+
+// spi0 finished
+extern "C" void __vector_17(void) __attribute__((signal, used, externally_visible));
+void __vector_17(void)
+{
+    spi0_state = spi0_state + 1;
+    switch (spi0_state)
+    {
+    case 1:
+
+        AdIcStatic_t::read();
+        AdIcStatic_t::template send<1>();
+        break;
+    case 2:
+        ad_raw_val_0 = static_cast<std ::uint16_t>(AdIcStatic_t::read()) << 8;
+        AdIcStatic_t::template send<2>();
+        break;
+    case 3:
+        ad_raw_val_0 |= static_cast<std ::uint16_t>(AdIcStatic_t::read());
+        spi0_state = 0;
+        AdIcStatic_t::disable();
+        break;
+    default:
+    }
 }
